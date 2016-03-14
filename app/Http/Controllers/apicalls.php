86927e0b91033026248d7fa94a37b6f675f0e038
Ómar Högni Guðmarsson <omar@eniac.is>
2016-03-14 19:23:48 +0000

    added a fetch movie function

--- app/Http/Controllers/apicalls.php
+++ app/Http/Controllers/apicalls.php
@@ -19,46 +19,60 @@ class apicalls extends Controller
 		// get like the data from like the user u know
 		$input = $request -> all();
 		$userid = DB::table('users')->where('api_token', $input['api_token'])->value('ID');; // i dont even have a clue how to get this
-		$imdblink = $input['link']; // it could be something like $_POST['imdb']; but no... lets not do that
-		$oldletter = "";
-		$index = 0;
-		$idfound = false;
-		for ($i=0; $i < strlen($imdblink); $i++) { 
-			if ($oldletter == $imdblink[$i] && $oldletter == "t" && is_numeric($imdblink[$i + 1])) {
-				// if there are two t's in a row then set the index of t to i -1 and substr 9 up.
-				$index = $i - 1;
-				$idfound = true;
-				break;
+		if (isset($userid)) {
+			$imdblink = $input['link']; // it could be something like $_POST['imdb']; but no... lets not do that
+			$oldletter = "";
+			$index = 0;
+			$idfound = false;
+			for ($i=0; $i < strlen($imdblink); $i++) { 
+				if ($oldletter == $imdblink[$i] && $oldletter == "t" && is_numeric($imdblink[$i + 1])) {
+					// if there are two t's in a row then set the index of t to i -1 and substr 9 up.
+					$index = $i - 1;
+					$idfound = true;
+					break;
+				}
+				$oldletter = $imdblink[$i];
 			}
-			$oldletter = $imdblink[$i];
-		}
-		if (!$idfound) {
-			return "could not find a valid id in that url";
+			if ($idfound) {
+				$uuid1 = Uuid::uuid1();
+				$output = substr($imdblink, $index, 9);
+				// get basic information from the omdb. The information we sotre in the database is just information that we display on the front page
+				// this is to save the time it takes to display the front page
+				$request = new Buzz\Message\Request('GET', '/', 'http://www.omdbapi.com/?i='.$output.'&tomatoes=true');
+				$response = new Buzz\Message\Response();
+				$client = new Buzz\Client\FileGetContents();
+				$client -> send($request,$response);
+				if (isset($response)) {
+					$thing = json_decode($response);
+					//return $thing -> imdbRating;
+					DB::table('submission')->insert([ // okei okei, he is like just inserting you know
+			    	['userID' => $userid, 'IMDB' => $output, 'ID' => $uuid1, 'moviename' => $thing -> Title, 'posterUrl' => $thing -> Poster, 'imdbRating' => $thing -> imdbRating] // that is not code i have ever seen before...
+					]);
+					return 1;
+					}
+					else{
+						return 0;
+					}
+				}
+			else{
+				return 0;
+			}
+		
 		}
 		else{
-		$uuid1 = Uuid::uuid1();
-		$output = substr($imdblink, $index, 9);
-		// return $output;
-		// insert like u know data into u know like a database
-
-		// get basic information from the omdb. The information we sotre in the database is just information that we display on the front page
-		// this is to save the time it takes to display the front page
-		$request = new Buzz\Message\Request('GET', '/', 'http://www.omdbapi.com/?i='.$output.'&tomatoes=true');
-		$response = new Buzz\Message\Response();
-		$client = new Buzz\Client\FileGetContents();
-		$client -> send($request,$response);
-		$thing = json_decode($response);
-		//return $thing -> imdbRating;
-		DB::table('submission')->insert([ // okei okei, he is like just inserting you know
-    	['userID' => $userid, 'IMDB' => $output, 'ID' => $uuid1, 'moviename' => $thing -> Title, 'posterUrl' => $thing -> Poster, 'imdbRating' => $thing -> imdbRating] // that is not code i have ever seen before...
-		]);
-		return 1;
-	}
+				return 0;
+			}
 	}
 	public function deletemovie(){
 
 	}
 	public function approvemovie(){
 
+	}
+	public function fetchmovies(){
+		// get the movies from the database this takes no arguments and fetches all the movies
+		// $users = DB::table('users')->select('name', 'email as user_email')->get();
+		$movies = DB::table('submission') -> select('moviename as name','posterUrl', 'imdbRating','ID','IMDB') -> get();
+		return json_encode($movies);
 	}
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,16 +15,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-
 // auth controller
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
     Route::get('/addmovie','HomeController@serveaddmovie');
     Route::get('/home', 'HomeController@index');
 });
-
 Route::group(['prefix' => 'api/', 'middleware' => 'auth:api'], function(){
 	// thesse routes are protected by the api_token. how ever the api_token is not being created when a user is created even though it was added to the modelfactory. users that are randomly generated however do get the api_token. 
 	Route::post('addmovie', 'apicalls@addmovie');
 });
+Route::post('api/fetchmovies','apicalls@fetchmovies');
